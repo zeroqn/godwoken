4f5dedcab2f9ef007726a52f233a86083f7eb170
Xuejie Xiao <xxuejie@gmail.com>
2021-03-12 02:11:25 +0000

    checkpoint: L2 chain syncing

--- crates/readonly-node/src/poller.rs
+++ crates/readonly-node/src/poller.rs
@@ -2,17 +2,31 @@ use crate::{
     indexer_types::{Order, Pagination, ScriptType, SearchKey, SearchKeyFilter, Tx},
     types::RunnerConfig,
 };
-use async_jsonrpc_client::{HttpClient, Output, Params as ClientParams, Transport, Value};
-use gw_chain::chain::Chain;
-use gw_jsonrpc_types::ckb_jsonrpc_types::{BlockNumber, Uint32};
-use gw_types::prelude::Unpack;
+use async_jsonrpc_client::{HttpClient, Output, Params as ClientParams, Transport};
+use ckb_types::{
+    core::ScriptHashType,
+    packed::{self as ckb_packed, Transaction},
+    prelude::Unpack as CkbUnpack,
+    H256,
+};
+use gw_chain::{
+    chain::{Chain, L1Action, L1ActionContext, SyncParam},
+    next_block_context::NextBlockContext,
+};
+use gw_jsonrpc_types::ckb_jsonrpc_types::{BlockNumber, HeaderView, TransactionWithStatus, Uint32};
+use gw_types::{
+    packed::{DepositionLockArgs, DepositionRequest, HeaderInfo},
+    prelude::*,
+};
 use parking_lot::RwLock;
+use serde::de::DeserializeOwned;
 use serde_json::{from_value, json};
 use std::sync::Arc;
+use std::time::SystemTime;
 
-fn to_result(output: Output) -> anyhow::Result<Value> {
+fn to_result<T: DeserializeOwned>(output: Output) -> anyhow::Result<T> {
     match output {
-        Output::Success(success) => Ok(success.result),
+        Output::Success(success) => Ok(from_value(success.result)?),
         Output::Failure(failure) => Err(anyhow::anyhow!("JSONRPC error: {}", failure.error)),
     }
 }
@@ -23,8 +37,9 @@ pub async fn poll_loop(
     ckb_rpc: String,
     indexer_rpc: String,
 ) -> anyhow::Result<()> {
-    let _ckb_client = HttpClient::new(&ckb_rpc)?;
+    let ckb_client = HttpClient::new(&ckb_rpc)?;
     let indexer_client = HttpClient::new(&indexer_rpc)?;
+    let mut chain_updater = ChainUpdater::new(Arc::clone(&chain), ckb_client, config.clone());
 
     loop {
         let tip_l1_block = chain.read().local_state().last_synced().number();
@@ -49,7 +64,12 @@ pub async fn poll_loop(
         let order = Order::Asc;
         let limit = Uint32::from(1000);
 
-        let mut txs: Pagination<Tx> = from_value(to_result(
+        // TODO: right now this logic does not handle forks well, we will need
+        // to tweak this.
+        // TODO: the syncing logic here works under the assumption that a single
+        // L1 CKB block can contain at most one L2 Godwoken block. The logic
+        // here needs revising, once we relax this constraint for more performance.
+        let mut txs: Pagination<Tx> = to_result(
             indexer_client
                 .request(
                     "get_transactions",
@@ -60,12 +80,12 @@ pub async fn poll_loop(
                     ])),
                 )
                 .await?,
-        )?)?;
-
+        )?;
         println!("L2 blocks to sync: {}", txs.objects.len());
+        chain_updater.update(&txs.objects).await?;
 
         while txs.objects.len() > 0 {
-            txs = from_value(to_result(
+            txs = to_result(
                 indexer_client
                     .request(
                         "get_transactions",
@@ -77,11 +97,209 @@ pub async fn poll_loop(
                         ])),
                     )
                     .await?,
-            )?)?;
+            )?;
 
             println!("L2 blocks to sync: {}", txs.objects.len());
+            chain_updater.update(&txs.objects).await?;
         }
 
         async_std::task::sleep(std::time::Duration::from_secs(1)).await;
     }
+}
+
+pub struct ChainUpdater {
+    pub chain: Arc<RwLock<Chain>>,
+    pub ckb_client: HttpClient,
+    pub last_tx_hash: Option<H256>,
+    pub runner_config: RunnerConfig,
+}
+
+impl ChainUpdater {
+    pub fn new(
+        chain: Arc<RwLock<Chain>>,
+        ckb_client: HttpClient,
+        runner_config: RunnerConfig,
+    ) -> ChainUpdater {
+        ChainUpdater {
+            chain,
+            ckb_client,
+            runner_config,
+            last_tx_hash: None,
+        }
+    }
+
+    pub async fn update(&mut self, txs: &[Tx]) -> anyhow::Result<()> {
+        for tx in txs {
+            self.update_single(&tx.tx_hash).await?;
+        }
+        Ok(())
+    }
+
+    async fn update_single(&mut self, tx_hash: &H256) -> anyhow::Result<()> {
+        if let Some(last_tx_hash) = &self.last_tx_hash {
+            if last_tx_hash == tx_hash {
+                return Ok(());
+            }
+        }
+        self.last_tx_hash = Some(tx_hash.clone());
+
+        let tx: Option<TransactionWithStatus> = to_result(
+            self.ckb_client
+                .request(
+                    "get_transaction",
+                    Some(ClientParams::Array(vec![json!(tx_hash)])),
+                )
+                .await?,
+        )?;
+        let tx_with_status =
+            tx.ok_or_else(|| anyhow::anyhow!("Cannot locate transaction: {:x}", tx_hash))?;
+        let tx: Transaction = tx_with_status.transaction.inner.into();
+        let block_hash = tx_with_status.tx_status.block_hash.ok_or_else(|| {
+            anyhow::anyhow!("Ttransaction {:x} is not committed on chain!", tx_hash)
+        })?;
+        let header_view: Option<HeaderView> = to_result(
+            self.ckb_client
+                .request(
+                    "get_header",
+                    Some(ClientParams::Array(vec![json!(block_hash)])),
+                )
+                .await?,
+        )?;
+        let header_view =
+            header_view.ok_or_else(|| anyhow::anyhow!("Cannot locate block: {:x}", block_hash))?;
+        let header_info = HeaderInfo::new_builder()
+            .number(header_view.inner.number.value().pack())
+            .block_hash(block_hash.0.pack())
+            .build();
+        let requests = self.extract_deposition_requests(&tx).await?;
+        let context = L1ActionContext::SubmitTxs {
+            deposition_requests: requests,
+        };
+        let update = L1Action {
+            transaction: tx,
+            header_info,
+            context,
+        };
+        let next_block_context = NextBlockContext {
+            aggregator_id: 0,
+            timestamp: SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .expect("Current time is before epoch, something is exploding...")
+                .as_secs(),
+        };
+        let sync_param = SyncParam {
+            reverts: vec![],
+            updates: vec![update],
+            next_block_context,
+        };
+        self.chain.write().sync(sync_param)?;
+        Ok(())
+    }
+
+    async fn extract_deposition_requests(
+        &self,
+        tx: &Transaction,
+    ) -> anyhow::Result<Vec<DepositionRequest>> {
+        let mut results = vec![];
+        for input in tx.raw().inputs().into_iter() {
+            // Load cell denoted by the transaction input
+            let tx_hash: H256 = input.previous_output().tx_hash().unpack();
+            let index = input.previous_output().index().unpack();
+            let tx: Option<TransactionWithStatus> = to_result(
+                self.ckb_client
+                    .request(
+                        "get_transaction",
+                        Some(ClientParams::Array(vec![json!(tx_hash)])),
+                    )
+                    .await?,
+            )?;
+            let tx_with_status =
+                tx.ok_or_else(|| anyhow::anyhow!("Cannot locate transaction: {:x}", tx_hash))?;
+            let tx: Transaction = tx_with_status.transaction.inner.into();
+            let cell_output = tx
+                .raw()
+                .outputs()
+                .get(index)
+                .ok_or_else(|| anyhow::anyhow!("OutPoint index out of bound"))?;
+            let cell_data = tx
+                .raw()
+                .outputs_data()
+                .get(index)
+                .ok_or_else(|| anyhow::anyhow!("OutPoint index out of bound"))?;
+
+            // Check if loaded cell is a deposition request
+            if let Some(deposition_request) =
+                try_parse_deposition_request(&cell_output, &cell_data, &self.runner_config)
+            {
+                results.push(deposition_request);
+            }
+        }
+        Ok(results)
+    }
+}
+
+fn try_parse_deposition_request(
+    cell_output: &ckb_packed::CellOutput,
+    cell_data: &ckb_packed::Bytes,
+    config: &RunnerConfig,
+) -> Option<DepositionRequest> {
+    if cell_output.lock().code_hash() != config.deployment_config.deposition_lock.code_hash()
+        || cell_output.lock().hash_type() != config.deployment_config.deposition_lock.hash_type()
+    {
+        return None;
+    }
+    let args = cell_output.lock().args().raw_data();
+    if args.len() < 32 {
+        return None;
+    }
+    let rollup_type_script_hash: [u8; 32] = config
+        .godwoken_config
+        .chain
+        .rollup_type_script
+        .calc_script_hash()
+        .unpack();
+    if args.slice(0..32) != &rollup_type_script_hash[..] {
+        return None;
+    }
+    let lock_args = match DepositionLockArgs::from_slice(&args.slice(32..)) {
+        Ok(lock_args) => lock_args,
+        Err(_) => return None,
+    };
+    // NOTE: In readoly mode, we are only loading on chain data here, timeout validation
+    // can be skipped. For generator part, timeout validation needs to be introduced.
+    let (amount, sudt_script) = match cell_output.type_().to_opt() {
+        Some(script) => {
+            if cell_data.len() < 16 {
+                return None;
+            }
+            let mut data = [0u8; 16];
+            data.copy_from_slice(&cell_data.raw_data()[0..16]);
+            (u128::from_le_bytes(data), script)
+        }
+        None => {
+            let script = ckb_packed::Script::new_builder()
+                .code_hash(
+                    ckb_packed::Byte32::new_builder()
+                        .set([ckb_packed::Byte::new(0); 32])
+                        .build(),
+                )
+                .hash_type(ScriptHashType::Data.into())
+                .args(
+                    ckb_packed::Bytes::new_builder()
+                        .extend(vec![ckb_packed::Byte::new(0); 32])
+                        .build(),
+                )
+                .build();
+            (0u128, script)
+        }
+    };
+    let capacity: u64 = cell_output.capacity().unpack();
+    let sudt_script_hash: [u8; 32] = sudt_script.calc_script_hash().unpack();
+    let deposition_request = DepositionRequest::new_builder()
+        .capacity(capacity.pack())
+        .amount(amount.pack())
+        .sudt_script_hash(sudt_script_hash.pack())
+        .script(lock_args.layer2_lock())
+        .build();
+    Some(deposition_request)
 }
--- crates/readonly-node/src/types.rs
+++ crates/readonly-node/src/types.rs
@@ -1,5 +1,9 @@
+use ckb_types::packed as ckb_packed;
 use gw_config::Config;
-use gw_jsonrpc_types::{ckb_jsonrpc_types::JsonBytes, parameter};
+use gw_jsonrpc_types::{
+    ckb_jsonrpc_types::{CellDep, JsonBytes, Script},
+    parameter,
+};
 use gw_types::{packed, prelude::*};
 
 // TODO: those are generic godwoken configs used across multiple tools,
@@ -13,8 +17,25 @@ pub enum JsonStoreConfig {
 
 #[derive(Clone, Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
 #[serde(rename_all = "snake_case")]
-#[serde(tag = "type")]
+pub struct JsonDeploymentConfig {
+    pub deposition_lock: Script,
+    pub custodian_lock: Script,
+    pub state_validator_lock: Script,
+    pub state_validator_type: Script,
+
+    pub deposition_lock_dep: CellDep,
+    pub custodian_lock_dep: CellDep,
+    pub state_validator_lock_dep: CellDep,
+    pub state_validator_type_dep: CellDep,
+
+    pub poa_state: Option<Script>,
+    pub poa_state_dep: Option<CellDep>,
+}
+
+#[derive(Clone, Serialize, Deserialize, PartialEq, Eq, Hash, Debug)]
+#[serde(rename_all = "snake_case")]
 pub struct JsonRunnerConfig {
+    pub deployment_config: JsonDeploymentConfig,
     pub godwoken_config: parameter::Config,
     pub store_config: JsonStoreConfig,
 }
@@ -25,7 +46,24 @@ pub enum StoreConfig {
 }
 
 #[derive(Clone, Debug)]
+pub struct DeploymentConfig {
+    pub deposition_lock: ckb_packed::Script,
+    pub custodian_lock: ckb_packed::Script,
+    pub state_validator_lock: ckb_packed::Script,
+    pub state_validator_type: ckb_packed::Script,
+
+    pub deposition_lock_dep: ckb_packed::CellDep,
+    pub custodian_lock_dep: ckb_packed::CellDep,
+    pub state_validator_lock_dep: ckb_packed::CellDep,
+    pub state_validator_type_dep: ckb_packed::CellDep,
+
+    pub poa_state: Option<ckb_packed::Script>,
+    pub poa_state_dep: Option<ckb_packed::CellDep>,
+}
+
+#[derive(Clone, Debug)]
 pub struct RunnerConfig {
+    pub deployment_config: DeploymentConfig,
     pub godwoken_config: Config,
     pub store_config: StoreConfig,
 }
@@ -42,9 +80,27 @@ impl From<JsonStoreConfig> for StoreConfig {
     }
 }
 
+impl From<JsonDeploymentConfig> for DeploymentConfig {
+    fn from(json: JsonDeploymentConfig) -> DeploymentConfig {
+        DeploymentConfig {
+            deposition_lock: json.deposition_lock.into(),
+            custodian_lock: json.custodian_lock.into(),
+            state_validator_lock: json.state_validator_lock.into(),
+            state_validator_type: json.state_validator_type.into(),
+            deposition_lock_dep: json.deposition_lock_dep.into(),
+            custodian_lock_dep: json.custodian_lock_dep.into(),
+            state_validator_lock_dep: json.state_validator_lock_dep.into(),
+            state_validator_type_dep: json.state_validator_type_dep.into(),
+            poa_state: json.poa_state.map(|state| state.into()),
+            poa_state_dep: json.poa_state_dep.map(|dep| dep.into()),
+        }
+    }
+}
+
 impl From<JsonRunnerConfig> for RunnerConfig {
     fn from(json: JsonRunnerConfig) -> RunnerConfig {
         RunnerConfig {
+            deployment_config: json.deployment_config.into(),
             godwoken_config: json.godwoken_config.into(),
             store_config: json.store_config.into(),
         }
