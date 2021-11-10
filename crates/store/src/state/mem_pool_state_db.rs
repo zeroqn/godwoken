5dff1f2bbca871cc2eb11f3e447dfa1850a96803
zero.qn <zero.qn@outlook.com>
2021-11-10 21:23:35 +0800

    feat(store): mem store for mem pool state

--- crates/store/src/state/mem_pool_state_db.rs
+++ crates/store/src/state/mem_pool_state_db.rs
@@ -1,34 +1,26 @@
 //! State DB
 
+use super::state_tracker::StateTracker;
+
 use crate::smt::mem_pool_smt_store::MemPoolSMTStore;
+use crate::transaction::mem_pool_store::MultiMemStore;
 use crate::{traits::KVStore, transaction::StoreTransaction};
+
 use anyhow::Result;
 use gw_common::{error::Error as StateError, smt::SMT, state::State, H256};
-use gw_db::error::Error;
-use gw_db::schema::{
-    COLUMN_DATA, COLUMN_MEM_POOL_DATA, COLUMN_MEM_POOL_SCRIPT, COLUMN_MEM_POOL_SCRIPT_PREFIX,
-    COLUMN_SCRIPT, COLUMN_SCRIPT_PREFIX,
-};
+use gw_db::schema::{COLUMN_DATA, COLUMN_SCRIPT, COLUMN_SCRIPT_PREFIX};
 use gw_traits::CodeStore;
-use gw_types::{
-    bytes::Bytes,
-    packed::{self, AccountMerkleState},
-    prelude::*,
-};
-
-use super::state_tracker::StateTracker;
+use gw_types::{bytes::Bytes, packed, prelude::*};
 
 pub struct MemPoolStateTree<'a> {
     tree: SMT<MemPoolSMTStore<'a>>,
-    account_count: u32,
     tracker: StateTracker,
 }
 
 impl<'a> MemPoolStateTree<'a> {
-    pub fn new(tree: SMT<MemPoolSMTStore<'a>>, account_count: u32) -> Self {
+    pub fn new(tree: SMT<MemPoolSMTStore<'a>>) -> Self {
         MemPoolStateTree {
             tree,
-            account_count,
             tracker: Default::default(),
         }
     }
@@ -37,27 +29,12 @@ impl<'a> MemPoolStateTree<'a> {
         &mut self.tracker
     }
 
-    pub fn get_merkle_state(&self) -> AccountMerkleState {
-        AccountMerkleState::new_builder()
-            .merkle_root(self.tree.root().pack())
-            .count(self.account_count.pack())
-            .build()
-    }
-
-    /// submit tree changes into memory block
-    /// notice, this function do not commit the DBTransaction
-    pub fn submit_tree_to_mem_block(&self) -> Result<(), Error> {
-        self.db()
-            .set_mem_block_account_smt_root(*self.tree.root())
-            .expect("set smt root");
-        self.db()
-            .set_mem_block_account_count(self.account_count)
-            .expect("set smt root");
-        Ok(())
-    }
-
     fn db(&self) -> &StoreTransaction {
-        self.tree.store().inner_store()
+        self.tree.store().db_store()
+    }
+
+    fn mem(&self) -> &MultiMemStore {
+        self.tree.store().mem_store()
     }
 }
 
@@ -70,83 +47,75 @@ impl<'a> State for MemPoolStateTree<'a> {
 
     fn update_raw(&mut self, key: H256, value: H256) -> Result<(), StateError> {
         self.tracker.touch_key(&key);
-        self.tree.update(key, value)?;
+        self.mem().smt().update_leaf(key, value);
         Ok(())
     }
 
     fn update_multi_raws(&mut self, pairs: Vec<(H256, H256)>) -> Result<(), StateError> {
         self.tracker.touch_keys(pairs.iter().map(|(k, _)| k));
-        self.tree.update_all(pairs)?;
+        for (key, value) in pairs {
+            self.update_raw(key, value)?;
+        }
         Ok(())
     }
 
     fn get_account_count(&self) -> Result<u32, StateError> {
-        Ok(self.account_count)
+        Ok(self.mem().get_account_count())
     }
 
     fn set_account_count(&mut self, count: u32) -> Result<(), StateError> {
-        self.account_count = count;
+        self.mem().set_account_count(count);
         Ok(())
     }
 
     fn calculate_root(&self) -> Result<H256, StateError> {
-        let root = self.tree.root();
-        Ok(*root)
+        unreachable!("should only calculate root in finalize thread mem state tree");
     }
 }
 
 impl<'a> CodeStore for MemPoolStateTree<'a> {
     fn insert_script(&mut self, script_hash: H256, script: packed::Script) {
-        self.db()
-            .insert_raw(
-                COLUMN_MEM_POOL_SCRIPT,
-                script_hash.as_slice(),
-                script.as_slice(),
-            )
-            .expect("insert script");
-
-        // build script_hash prefix search index
-        self.db()
-            .insert_raw(
-                COLUMN_MEM_POOL_SCRIPT_PREFIX,
-                &script_hash.as_slice()[..20],
-                script_hash.as_slice(),
-            )
-            .expect("insert script prefix");
+        self.mem().insert_script(script_hash, script);
+        self.mem()
+            .insert_short_address(&script_hash.as_slice()[..20], script_hash);
     }
 
     fn get_script(&self, script_hash: &H256) -> Option<packed::Script> {
+        if let Some(script) = self.mem().get_script(script_hash) {
+            return Some(script);
+        }
+
         self.db()
-            .get(COLUMN_MEM_POOL_SCRIPT, script_hash.as_slice())
-            .or_else(|| self.db().get(COLUMN_SCRIPT, script_hash.as_slice()))
-            .map(|slice| packed::ScriptReader::from_slice_should_be_ok(slice.as_ref()).to_entity())
+            .get(COLUMN_SCRIPT, script_hash.as_slice())
+            .map(|s| packed::ScriptReader::from_slice_should_be_ok(s.as_ref()).to_entity())
     }
 
     fn get_script_hash_by_short_address(&self, script_hash_prefix: &[u8]) -> Option<H256> {
-        match self
-            .db()
-            .get(COLUMN_MEM_POOL_SCRIPT_PREFIX, script_hash_prefix)
-            .or_else(|| self.db().get(COLUMN_SCRIPT_PREFIX, script_hash_prefix))
-        {
-            Some(slice) => {
+        let mem = self.mem();
+        if let Some(hash) = mem.get_script_hash_by_short_address(script_hash_prefix) {
+            return Some(hash);
+        }
+
+        self.db()
+            .get(COLUMN_SCRIPT_PREFIX, script_hash_prefix)
+            .map(|slice| {
                 let mut hash = [0u8; 32];
                 hash.copy_from_slice(slice.as_ref());
-                Some(hash.into())
-            }
-            None => None,
-        }
+                hash.into()
+            })
     }
 
     fn insert_data(&mut self, data_hash: H256, code: Bytes) {
-        self.db()
-            .insert_raw(COLUMN_MEM_POOL_DATA, data_hash.as_slice(), &code)
-            .expect("insert data");
+        self.mem().insert_data(data_hash, code);
     }
 
     fn get_data(&self, data_hash: &H256) -> Option<Bytes> {
+        if let Some(data) = self.mem().get_data(data_hash) {
+            return Some(data);
+        }
+
         self.db()
-            .get(COLUMN_MEM_POOL_DATA, data_hash.as_slice())
-            .or_else(|| self.db().get(COLUMN_DATA, data_hash.as_slice()))
+            .get(COLUMN_DATA, data_hash.as_slice())
             .map(|slice| Bytes::from(slice.to_vec()))
     }
 }
