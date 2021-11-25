dc64c00d2729d2e7bcba9c912ca1d11130c12756
zero.qn <zero.qn@outlook.com>
2021-11-25 10:56:17 +0800

    chore(block producer): use jemalloc

--- crates/block-producer/src/main.rs
+++ crates/block-producer/src/main.rs
@@ -1,3 +1,10 @@
+#[cfg(not(target_env = "msvc"))]
+use tikv_jemallocator::Jemalloc;
+
+#[cfg(not(target_env = "msvc"))]
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
+
 use anyhow::{Context, Result};
 use clap::{App, Arg, SubCommand};
 use gw_block_producer::{db_block_validator, runner};
