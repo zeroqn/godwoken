85a28b0b9cde8b4ba95e84a41574ca661728ab64
zero.qn <zero.qn@outlook.com>
2021-12-09 13:47:02 +0800

    debug

--- crates/block-producer/src/runner.rs
+++ crates/block-producer/src/runner.rs
@@ -486,6 +486,11 @@ pub fn run(config: Config, skip_config_check: bool) -> Result<()> {
                 log::info!("account 3953 exit in block {}", block);
                 break;
             }
+            let script_hash = tree.get_script_hash(3954)?;
+            if script_hash != H256::zero() {
+                log::info!("account 3954 exit in block {}", block);
+                break;
+            }
             block = block.saturating_add(1);
             if block == 45500 {
                 break;
